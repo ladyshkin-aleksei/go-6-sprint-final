b2beba9df3d80c0ed5b4980cf1384c22e3357065
ladyshkin-aleksei <ladyshkin2000@mail.ru>
2025-11-06 17:01:50 +0300

    second commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -17,14 +17,14 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	file, err := os.Open("../index.html")
+	file, err := os.Open("index.html")
 	if err != nil {
 		http.Error(w, "file index.html not found: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
 	_, err = io.Copy(w, file)
 	if err != nil {
